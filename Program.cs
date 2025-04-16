3a1d9129c831d2b2bce383630e3fb9eb46969dc4
itodobien <daniel.w.garcia@outlook.com>
2025-04-16 14:55:51 -0700

    Complete solution
    most succinct way I could solve it

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Please enter a number:");
+
+int.TryParse(Console.ReadLine(), out int number);
+
+int[] array = new int[number];
+
+for (int i = 0; i < number; i++)
+{
+    array[i] = number;
+}
+
+while (array.Length > 0)
+{
+    Console.WriteLine(string.Join(" ", array));
+    
+    Array.Resize(ref array, array.Length - 1);
+}
